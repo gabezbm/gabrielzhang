09d636860ead4f76d4fcb5bfc100f1d3674cc455
Gary Zhang <garyzhang2002@hotmail.com>
2024-03-11 01:49:02 -0700

    small change on scripts

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  fetch(
-    "https://taylor-swift-api.sarbo.workers.dev/lyrics?numberOfParagraphs=1?shouldRandomizeLyrics=true",
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      document.getElementById("quote").innerText = data.lyrics[0];
-    });
+    fetch(
+        "https://taylor-swift-api.sarbo.workers.dev/lyrics?numberOfParagraphs=1?shouldRandomizeLyrics=true",
+    )
+        .then((response) => response.json())
+        .then((data) => {
+            document.getElementById("quote").innerText = data.lyrics[0];
+        })
+        .catch(() => {
+            document.getElementById("quote").innerText = "[Failed to fetch the lyrics.]";
+        });
 });
